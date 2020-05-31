3c02877081367a37ee0b081ca1726e55a9649b7b
Myke Oliveira <38540828+mykeoliveira@users.noreply.github.com>
2020-05-31 15:45:26 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,8 +1,8 @@
 <?php
-$dbname='REA';
-$usuario='myke1';
-$password='';
-$localhost='localhost';
+$dbname='heroku_3916b5627a97d8c';
+$usuario='be5e2232b15f70';
+$password='e3eeddaa';
+$localhost='us-cdbr-east-05.cleardb.net';
 
 //Passo 1 - Conecta ao servidor MySQL
 if(!($id = mysqli_connect($localhost,$usuario,$password,$dbname))) {
