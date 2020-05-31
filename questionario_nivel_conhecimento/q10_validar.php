e89768e2a9cf75fa2adc8e8a9b0348b8e14d9613
Myke Oliveira <38540828+mykeoliveira@users.noreply.github.com>
2020-05-31 18:14:30 -0300

    Update q10_validar.php

--- questionario_nivel_conhecimento/q10_validar.php
+++ questionario_nivel_conhecimento/q10_validar.php
@@ -6,15 +6,18 @@ include ("../conecta.php");
 include ("../seguranca.php");
 
 //session_start();
-  $numero = $_SESSION['login'];
-  $id = $_SESSION["id"];
+$numero = $_SESSION['login'];
+$id = $_SESSION["id"];
 $porcentagem = $_POST["porcentagem"];
 
 //echo $porcentagem;
-
+$dbname='heroku_3916b5627a97d8c';
+$usuario='be5e2232b15f70';
+$password='e3eeddaa';
+$localhost='us-cdbr-east-05.cleardb.net';
 //$id_session = $_SESSION['usuarioID'];
 
-$conexao = mysqli_connect("localhost","myke1","","REA") or die ('Não foi possível conectar');
+$conexao = mysqli_connect($localhost,$usuario,$password,$dbname) or die ('Não foi possível conectar');
 
 $sql_update = "UPDATE resultado SET resultadodez='$porcentagem' WHERE id_usuario_resp='$id'";
 
